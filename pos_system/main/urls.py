8c974314efb9db765b7527c5eb4c67f7c3c2155e
Burakherdogan <erdoganburakhasan@gmail.com>
2024-02-23 23:59:33 +0300

    shopping cart updates

--- pos_system/main/urls.py
+++ pos_system/main/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('reports', views.reports, name="reports"),
+    path('', views.shopping_cart, name="shopping_cart"),
+    path('add/', views.cart_add, name="cart_add"),
+    path('delete', views.cart_delete, name="cart_delete"),
+    path('update', views.cart_update, name="cart_update"),
+
 ]
--- pos_system/main/views.py
+++ pos_system/main/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Products
+from .cart import Cart
+from django.http import JsonResponse
+
 
 def index(request):
     products = {
@@ -9,3 +12,29 @@ def index(request):
 
 def reports(request):
     return render(request, "main/reports.html")
+
+def shopping_cart(request):
+    return render(request, "shopping_cart.html",{})
+
+def cart_add(request):
+    # get the cart
+    cart = Cart(request)
+    # test post
+    if request.POST.get('action') == 'post':
+        # get stuff
+        product_id = int(request.POST.get('product_id'))
+        # lookup product in DB
+        product = get_object_or_404(Products, id=product_id)
+        # save to session
+        cart.add(product=product)
+        #get cart quantity
+        cart_quantity = cart.__len__()
+        # return response
+        response = JsonResponse({'qty': cart_quantity})
+        return response
+    
+def cart_delete(request):
+    pass
+
+def cart_update(request):
+    pass
